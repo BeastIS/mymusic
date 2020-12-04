57ffe3e4479673e35328f5f2eed0c249bab47295
BeastIS <64451094+BeastIS@users.noreply.github.com>
2020-12-04 21:12:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,126 +1,24 @@
-/* eslint-disable */
+$(function()
+{
+    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['Dawn','Me & You','Electro Boy','Home','Proxy (Original Mix)'], trackNames = ['Skylike - Dawn','Alex Skrindo - Me & You','Kaaze - Electro Boy','Jordan Schor - Home','Martin Garrix - Proxy'], albumArtworks = ['_1','_2','_3','_4','_5'], trackUrl = ['https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3'], playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
 
-document.addEventListener('DOMContentLoaded', () => {
-	// SELECT ELEMENTS
-	let forwardBtn = document.querySelector('.forward-btn');
-	let backwardBtn = document.querySelector('.backward-btn');
-	let songTitle = document.querySelector('.title-wrapper h1');
-	let artistTitle = document.querySelector('.title-wrapper h2');
-
-	// CREATE ARRAY FOR CONTROLS
-	let controlArray = [forwardBtn, backwardBtn];
-
-	// ADD EVENTLISTENERS TO CONTROLS AND CALL FUNCTIONS
-	controlArray.forEach((control) => {
-		control.addEventListener('click', () => {
-			switch (control) {
-				case forwardBtn:
-					nextSong();
-					break;
-
-				case backwardBtn:
-					prevSong();
-					break;
-			}
-		})
-	})
-
-	// MUSIC INFORMATION
-	let songArray = ['Columbiana', 'MONEYHONEYDRIP', 'Diggiduwaist', 'You', 'Dystopia'];
-	let artistArray = ['Capital Bra & Samra', 'Jamule', 'Selmon', 'Besomorph & Jagsy', 'Disarstar'];
-
-	// SET INTIAL SONG NUMBER + LOAD IN ANIMATION
-	let songNumber = 2;
-	songTitle.innerHTML = songArray[songNumber];
-
-	anime({
-		targets: '.Diggiduwaist',
-		scale: [0.8, 1.1],
-		easing: 'easeOutExpo',
-		duration: 1500,
-	})
-
-	function nextSong() {
-		if (songNumber == 4) {
-			songNumber = 0;
-			setSong();
-
-			// ANIMATE TO FIRST COVER WHEN END OF songArray REACHED
-			anime({
-				targets: '.cover-row',
-				translateX: '510px',
-				easing: 'easeOutExpo',
-			})
-		} else {
-			songNumber += 1;
-			setSong();
-			scaleCoverDownNext();
-
-			// ANIMATE TO NEXT COVER
-			anime({
-				targets: '.cover-row',
-				translateX: '-=255px',
-				easing: 'easeOutExpo',
-			})
-		}
-	}
-
-	function prevSong() {
-		if (songNumber == 0) {
-			// DO NOTHING WHEN PREV IS CLICKED AT BEGINNING OF LIST
-		} else {
-			songNumber -= 1;
-			setSong();
-			scaleCoverDownPrev();
-
-			// ANIMATE TO NEXT COVER
-			anime({
-				targets: '.cover-row',
-				translateX: '+=255px',
-				easing: 'easeOutExpo',
-			})
-		}
-	}
-
-	// SET SONG INFORMATION AND COVER
-	function setSong() {
-		songTitle.innerHTML = songArray[songNumber];
-		artistTitle.innerHTML = artistArray[songNumber];
-
-		// SCALE UP CURRENT COVER
-		anime({
-			targets: `.${songArray[songNumber]}`,
-			scale: 1.1,
-			easing: 'easeOutExpo',
-			duration: 1500,
-		})
-	}
-
-	function scaleCoverDownNext() {
-		let prevSongNumber = songNumber - 1;
-
-		anime({
-			targets: `.${songArray[prevSongNumber]}`,
-			scale: 1,
-			easing: 'easeOutExpo',
-			duration: 1500,
-		})
-	}
-
-	function scaleCoverDownPrev() {
-		let nextSongNumber = songNumber + 1;
-
-		anime({
-			targets: `.${songArray[nextSongNumber]}`,
-			scale: 1,
-			easing: 'easeOutExpo',
-			duration: 1500,
-		})
-	}
-
-	// PLAY/PAUSE BUTTON
-	let playBtn = document.querySelector('.play-icon');
-	playBtn.addEventListener('click', () => {
-		playBtn.classList.toggle('paused');
-	})
-})
+    function playPause()
+    {
+        setTimeout(function()
+        {
+            if(audio.paused)
+            {
+                playerTrack.addClass('active');
+                albumArt.addClass('active');
+                checkBuffering();
+                i.attr('class','fas fa-pause');
+                audio.play();
+            }
+            else
+            {
+                playerTrack.removeClass('active');
+                albumArt.removeClass('active');
+                clearInterval(buffInterval);
+                albumArt.removeClass('buffering');
+                i.attr('class','fas fa-play');
+                audio.pause();
